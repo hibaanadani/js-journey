604d30337f643ae72374b7c638e5605c6b3963e7
hibaanadani <hibaanadani1@gmail.com>
2025-04-17 00:06:43 +0300

    deposit method created

--- Classes.js
+++ Classes.js
@@ -3,4 +3,13 @@ class BankAccount{
         this.ownerName = ownerName;
         this.balance = initialBalance;
     }
+    deposit(amount){
+        if(amount > 0){
+            this.balance += amount;
+            console.log(`Deposited ${amount}. New balance is ${this.balance}.`);
+        }
+        else{
+            console.log("Deposit amount must be positive.");
+        }
+}
 }
