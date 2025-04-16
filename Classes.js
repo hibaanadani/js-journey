f821ac71b142e847cf411205abcc16c61af06db1
hibaanadani <hibaanadani1@gmail.com>
2025-04-17 00:08:56 +0300

    withdraw  method created

--- Classes.js
+++ Classes.js
@@ -11,5 +11,14 @@ class BankAccount{
         else{
             console.log("Deposit amount must be positive.");
         }
-}
+    }
+    withdraw(amount){
+        if(amount > 0 && amount <= this.balance){
+            this.balance -= amount;
+            console.log(`Withdrew ${amount}. New balance is ${this.balance}.`);
+        }
+        else{
+            console.log("Withdrawal amount must be positive and less than or equal to the balance.");
+        }
+    }
 }
