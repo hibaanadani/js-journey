b92a54321aefd20a73e664b076ddafd1e8e52fed
hibaanadani <hibaanadani1@gmail.com>
2025-04-17 00:28:24 +0300

    Added history method and added the push into histroy in other methods

--- Classes.js
+++ Classes.js
@@ -3,9 +3,17 @@ class BankAccount{
         this.ownerName = ownerName;
         this.balance = initialBalance;
     }
+    getHistory(){
+        if(!this.history){
+            this.history=[]
+        }
+        return this.history;
+    }
+
     deposit(amount){
         if(amount > 0){
             this.balance += amount;
+            this.getHistory().push('Deposited ${amount}');
             console.log(`Deposited ${amount}. New balance is ${this.balance}.`);
         }
         else{
@@ -15,10 +23,25 @@ class BankAccount{
     withdraw(amount){
         if(amount > 0 && amount <= this.balance){
             this.balance -= amount;
+            this.getHistory().push('Withdrew ${amount}');
             console.log(`Withdrew ${amount}. New balance is ${this.balance}.`);
         }
         else{
             console.log("Withdrawal amount must be positive and less than or equal to the balance.");
         }
+    }
+    TransferTo(anotherAccount, amount){
+        if(amount > 0 && amount <= this.balance){
+            this.withdraw(amount);
+            anotherAccount.deposit(amount);
+            this.getHistory().push('Transferred ${amount} to ${anotherAccount.ownerName}');
+            console.log(`Transferred ${amount} to ${anotherAccount.ownerName}.`);
+        }
+        else{
+            console.log("Transfer amount must be positive and less than or equal to the balance.");
+        }
+    }
+    getSummary(){
+        return `${this.ownerName}'s balance is ${this.balance}`;
     }
 }
