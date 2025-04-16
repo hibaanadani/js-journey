855688a1a9c2ae23ad6071295c79ad2ffeb8b344
hibaanadani <hibaanadani1@gmail.com>
2025-04-17 01:00:47 +0300

    getting the elements from html by their class name

---
+++ todo list/script.js
@@ -0,0 +1,4 @@
+//get the elements from the DOM by their class names
+const taskInput = document.querySelector(".task-input");
+const addTaskButton = document.querySelector(".add-btn");
+const taskList = document.querySelector(".task-list");
