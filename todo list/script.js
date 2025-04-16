6e5c4f2c5be234650de418151e36ee5b05a14eb5
hibaanadani <hibaanadani1@gmail.com>
2025-04-17 01:11:35 +0300

    add task function added

--- todo list/script.js
+++ todo list/script.js
@@ -1,4 +1,17 @@
-//get the elements from the DOM by their class names
+//get the elements by their class names
 const taskInput = document.querySelector(".task-input");
 const addTaskButton = document.querySelector(".add-btn");
 const taskList = document.querySelector(".task-list");
+
+function addTask() {
+    const taskText = taskInput.value.trim(); // Get input text & remove extra spaces
+    if (taskText === "") {  // Prevent empty tasks
+        alert("Please enter a task.");
+        return;
+    }
+    const li = document.createElement("li"); // Create a new list item
+    li.textContent = taskText; // Set its text to the input value
+    
+    taskList.appendChild(li); // Add task to the list
+    taskInput.value = ""; // Clear input after adding the task
+    }
